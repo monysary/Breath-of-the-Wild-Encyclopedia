2a833bf27faf8710bc5924736e27648c9ce219dc
monysary <sary.mony@gmail.com>
2023-04-11 19:56:49 -0700

    added styling and background to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,16 @@
+import background from '../images/The-Art-of-The-Legend-of-Zelda-Breath-of-the-Wild-3.jpg'
+
 import Header from "./components/Header";
 
 function Homepage() {
     return (
-        <div>
+        <div style={{
+            height: '100vh',
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover'
+
+        }} 
+        >
             <Header />
         </div>
     )
